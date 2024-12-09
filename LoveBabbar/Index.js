fd4605fd6edb23ca682e50b762333ba1da93b2e1
Harry101969 <harshagarwal19696@gmail.com>
2024-12-09 15:18:17 +0530

    Added Easier way to find prime numbers in a given range

--- LoveBabbar/Index.js
+++ LoveBabbar/Index.js
@@ -26,3 +26,67 @@ function sums(N) {
   console.log(newNum);
 }
 sums(6);
+console.log("Factorial");
+function factorial(N) {
+  let num = 1;
+  for (let i = 1; i <= N; i++) {
+    num = num * i;
+  }
+  console.log(num);
+}
+factorial(3);
+
+function checkPrime(N) {
+  if (N <= 1) {
+    console.log("Not a valid Number");
+    return;
+  }
+
+  for (let i = 2; i <= Math.sqrt(N); i++) {
+    if (N % i === 0) {
+      console.log(N, "Not a prime number");
+      return;
+    }
+  }
+  console.log(N, "Is a prime number!");
+}
+checkPrime(113);
+
+function rangedPrime(N) {
+  for (let i = 2; i <= N; i++) {
+    let isPrime = true;
+
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        console.log(i, "Is not a prime number!");
+      }
+    }
+    if (isPrime) {
+      console.log(i, "Is a prime number!");
+    }
+  }
+}
+rangedPrime(10);
+
+console.log("A better and earier code below ");
+function rangePrime(N) {
+  function isPrime(N) {
+    for (let i = 2; i < N; i++) {
+      if (N % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for (let i = 2; i <= N; i++) {
+    if (isPrime(i)) {
+      console.log(i, "Is a prime number");
+    } else {
+      console.log(i, "Is not a prime number");
+    }
+  }
+}
+
+rangePrime(10);
